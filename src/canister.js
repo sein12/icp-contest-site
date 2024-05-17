d827153c1b63ecc1f9f1620482ecc2b81a5dc45a
sein12 <moses1008@naver.com>
2024-05-17 16:31:24 +0900

    second commit

--- src/canister.js
+++ src/canister.js
@@ -1,12 +1,24 @@
-import { Actor, HttpAgent, HttpMethod } from "@dfinity/agent";
+import { Actor, HttpAgent } from "@dfinity/agent";
 import { idlFactory } from "../../azle_test/src/backend/index.did"; // 경로를 적절히 조정하세요
 const canisterId = process.env.REACT_APP_CANISTER_ID;
 
-// HttpAgent 생성
 const agent = new HttpAgent();
 const canisterActor = Actor.createActor(idlFactory, {
     agent,
     canisterId,
 });
+
+export async function fetchMessages() {
+    const response = await fetch(`${process.env.REACT_APP_CANISTER_ORIGIN}/messages`);
+    return response.json();
+}
+
+export async function sendMessage(user, message) {
+    await fetch(`${process.env.REACT_APP_CANISTER_ORIGIN}/messages`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user, message }),
+    });
+}
 
 export { canisterActor };
